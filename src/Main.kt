36ef731c4b181946af7f3e7da63800d1d7f7a526
Josh LaSota <jlasota126@students.mchenry.edu>
2024-01-24 10:29:24 -0600

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val a = 10
+    val b = 5
+
+    val resultAdd = a + b
+    println("$a + $b = $resultAdd")
+
+    val resultSub = a - b
+    println("$a - $b = $resultSub")
+
+    val resultMul = a * b
+    println("$a * $b = $resultMul")
+
+    val resultDiv = a / b
+    println("$a / $b = $resultDiv")
+
+    val resultMod = a % b
+    println("$a % $b = $resultMod")
+}
